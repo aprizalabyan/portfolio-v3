c3da67a4a0df46baa56d6a77968b1f193ad4057a
aprizalabyan <abi@jkt1.ebdesk.com>
2025-01-21 15:59:30 +0700

    define colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,26 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "accent-blue": "#59CAEE",
+        "accent-blue-darken-1": "#25B7E4",
+        "accent-green": "#5EEAD4",
+        "primary-text": "#E2E8F0",
+        "secondary-text": "#94A3B8",
+        "secondary-darken-text": "#64748B",
+        "dark-base": "#1A191D",
+        "dark-lighten-1": "#202022",
+        "dark-lighten-2": "#313438",
+        "dark-lighten-3": "#555759",
+        "hover": "rgba(45, 152, 212, 0.05)"
       },
     },
   },
