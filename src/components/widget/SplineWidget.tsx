b211bbe5a75317be289d99e9e29e93aa85eea63e
aprizalabyan <abi@jkt1.ebdesk.com>
2025-02-17 18:46:48 +0700

    stop scene animation on mobile-device

--- src/components/widget/SplineWidget.tsx
+++ src/components/widget/SplineWidget.tsx
@@ -4,11 +4,32 @@ import Spline from '@splinetool/react-spline';
 import React, { useEffect, useRef, useState } from 'react';
 
 export default function SplineWidget() {
-  const splineInstance = useRef({})
+  const splineInstance = useRef<any>({})
+  const [isMobile, setIsMobile] = useState(false);
+  const [isLoad, setIsLoad] = useState(false);
 
-  function onLoadSpline(splineApp: any) {
+  async function onLoadSpline(splineApp: any) {
     splineInstance.current = splineApp
+    setIsLoad(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   }
+
+  function stop() {
+    if (splineInstance.current)
+      splineInstance.current.stop()
+  }
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    setIsMobile(/android|iphone|ipad|ipod/i.test(userAgent));
+  }, []);
+
+  useEffect(() => {
+    if (isMobile && isLoad)
+      setTimeout(() => {
+        stop()
+      }, 1000);
+  }, [isMobile, isLoad])
 
   return (
     <div className="h-96 -mx-6 mt-12 lg:mx-0 lg:mt-0">
