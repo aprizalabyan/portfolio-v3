2b1e0ff6f4f058e164a44eb451d277a8c43d5ff9
aprizalabyan <abi@jkt1.ebdesk.com>
2025-02-18 16:46:14 +0700

    show project-description on table all-project

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { DataTable } from "primereact/datatable"
 import { Column } from "primereact/column"
@@ -17,18 +16,24 @@ export default function Project() {
   const router = useRouter()
 
   const titleBodyTemplate = (item: any) => {
-    return <div className="flex gap-4 items-center">
-      <img src={item.image} alt="img" className="w-[60px] h-[40px] object-cover" />
-      <span className="text-sm hidden md:block">{item.title}</span>
-      <a href={item.url} target="_blank" className="text-sm block md:hidden group/link">
-        {item.title}
-        <i className="text-xs pi pi-arrow-up-right ml-1 mb-0.5 transition-transform group-hover/link:translate-x-1 group-hover/link:-translate-y-1"></i>
-      </a>
+    return <div className="flex gap-4 items-center pe-4">
+      <img src={item.image} alt="img" className="w-[60px] h-[40px] min-w-[60px] object-cover" />
+      <div className="flex flex-col">
+        <span className="text-sm hidden md:block">{item.title}</span>
+        {item.url !== "" ?
+          <a href={item.url} target="_blank" className="text-sm block md:hidden group/link">
+            {item.title}
+            <i className="text-xs pi pi-arrow-up-right ml-1 mb-0.5 transition-transform group-hover/link:translate-x-1 group-hover/link:-translate-y-1"></i>
+          </a> :
+          <span className="text-sm block md:hidden">{item.title}</span>
+        }
+        <span className="text-xs text-secondary-text hidden md:block">{item.description}</span>
+      </div>
     </div>
   }
 
   const tagBodyTemplate = (item: any) => {
-    return <div className="flex flex-wrap gap-1.5">
+    return <div className="flex flex-wrap gap-1.5 pe-4">
       {item.tags.map((tag: any, i: number) => (
         <Chip
           key={i}
@@ -41,16 +46,17 @@ export default function Project() {
   }
 
   const linkBodyTemplate = (item: any) => {
-    return <a
-      href={item.url}
-      className="flex items-end text-sm text-secondary-text hover:text-accent-blue group/link"
-      target="_blank"
-    >
-      <span>
-        {item.url}
-        <i className="text-xs pi pi-arrow-up-right ml-1 mb-0.5 transition-transform group-hover/link:translate-x-1 group-hover/link:-translate-y-1"></i>
-      </span>
-    </a>
+    if (item.url !== "")
+      return <a
+        href={item.url}
+        className="flex items-end text-sm text-secondary-text hover:text-accent-blue group/link pe-2"
+        target="_blank"
+      >
+        <span>
+          {item.url}
+          <i className="text-xs pi pi-arrow-up-right ml-1 mb-0.5 transition-transform group-hover/link:translate-x-1 group-hover/link:-translate-y-1"></i>
+        </span>
+      </a>
   }
 
   return (
